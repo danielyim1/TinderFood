9cd008f1bf99e329ab0d6ee8535ab7fd116c034d
danielyim1 <dyim2@jhu.edu>
2021-01-05 21:17:35 -0500

    basic files

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
-import Counter from './Counter';
 
-class App extends Component {
-  //hi mandy
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <Counter buttonText='hi'/>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button> 
+          Left!
+          </button>
+          <button>
+            Right!
+            </button>
+        <p >Tinder For Food!! 
+        </p>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tinder For Food!
+        </a> */}
+      </header>
+    </div>
+  );
 }
 
 export default App;
